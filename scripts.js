8c5175a36eabd688f84e93f7de9935110012b2ae
noob <syntex1147@gmail.com>
2023-12-30 20:14:24 -0500

    -

---
+++ scripts.js
@@ -0,0 +1,8 @@
+window.onload = function () {
+    var msnry = new Masonry('.gallery', {
+      itemSelector: '.item',
+      columnWidth: '.grid-sizer',
+      percentPosition: true
+    });
+  };
+  
